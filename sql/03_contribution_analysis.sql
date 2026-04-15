6f0109bbb2aab84b3e6ba6b3cbda588e04d06ef1
MinkyoungP <mkpark787@gmail.com>
2026-04-15 14:10:39 -0700

    Update 03_contribution_analysis.sql

--- sql/03_contribution_analysis.sql
+++ sql/03_contribution_analysis.sql
@@ -1,1 +1,11 @@
+-- Contribution analysis by customer segment
 
+SELECT
+    segment,
+    SUM(revenue) AS total_revenue,
+    SUM(marketing_cost) AS total_marketing_cost,
+    SUM(variable_cost) AS total_variable_cost,
+    SUM(revenue - marketing_cost - variable_cost) AS contribution_margin
+FROM core_relief_data
+GROUP BY segment
+ORDER BY contribution_margin DESC;
